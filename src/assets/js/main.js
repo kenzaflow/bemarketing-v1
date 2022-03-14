3bc2d26ed90e751dfb9aff555c1754794b15c4d9
Kevin Zaracho <kevin.isap99@gmail.com>
2022-03-14 08:44:19 -0300

    agregué el formulario, entre otras

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,6 +12,48 @@ const debounce = (callback, wait) => {
     }
 }
 
+const initForm = () => {
+    let form = document.getElementById('my-form')
+
+    if (!form) return null
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        let status = document.getElementById('my-form-status')
+        let data = new FormData(event.target)
+        fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                Accept: 'application/json',
+            },
+        })
+            .then((response) => {
+                if (response.ok) {
+                    status.innerHTML = 'Thanks for your submission!'
+                    form.reset()
+                } else {
+                    response.json().then((data) => {
+                        if (Object.hasOwn(data, 'errors')) {
+                            status.innerHTML = data['errors']
+                                .map((error) => error['message'])
+                                .join(', ')
+                        } else {
+                            status.innerHTML =
+                                'Oops! There was a problem submitting your form'
+                        }
+                    })
+                }
+            })
+            .catch((error) => {
+                status.innerHTML =
+                    'Oops! There was a problem submitting your form'
+            })
+    }
+    
+    form.addEventListener('submit', handleSubmit)
+}
+
 /* Short - addEventListener */
 
 /* Main.js */
@@ -29,4 +71,6 @@ window.addEventListener('DOMContentLoaded', () => {
             if (window.scrollY > 0) topBar.classList.add('has-scroll')
         }, 10)
     )
+
+    initForm()
 })
