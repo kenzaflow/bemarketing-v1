2f0165fa9799edf662488e34e85c61f035816659
Kevin Zaracho <kevin.isap99@gmail.com>
2022-03-29 14:27:29 -0300

    Listo sección Equipo

--- src/assets/js/modules/scroll.js
+++ src/assets/js/modules/scroll.js
@@ -1,5 +1,5 @@
-const children = document.querySelectorAll('.scroll-snap-children')
-const parent = document.querySelector('.scroll-snap-parent')
+const parent = document.querySelector('.team .body')
+const children = parent.querySelector('.person')
 
 let startX
 let scrollLeft
@@ -11,8 +11,32 @@ export function initScroll() {
     parent.addEventListener('mouseleave', (e) => mouseLeave(e))
     parent.addEventListener('mousemove', (e) => mouseMove(e))
 
-    parent.onwheel = (event) => {
-        parent.scrollBy(0, event.deltaX)
+    document.querySelector('.next-person').onclick = (event) => {
+        handleNavegation(1)
+    }
+    document.querySelector('.prev-person').onclick = (event) => {
+        handleNavegation(-1)
+    }
+}
+
+const childrenSize = () => {
+    return (
+        children.clientWidth +
+        Number(
+            window
+                .getComputedStyle(children)
+                .margin.split(' ')[1]
+                .replace('px', '')
+        ) *
+            2
+    )
+}
+
+function handleNavegation(x) {
+    if (x > 0) {
+        parent.scrollBy(childrenSize(), 0)
+    } else {
+        parent.scrollBy(-childrenSize(), 0)
     }
 }
 
@@ -31,7 +55,6 @@ function mouseLeave(e) {
 }
 function mouseMove(e) {
     if (isDown) {
-        e.preventDefault()
         //Move horizontally
         const x = e.pageX - parent.offsetTop
         const walkX = (x - startX) * 2
